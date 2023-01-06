3acf22c10365373bf93ab3a69c291c6801f40e02
Adam Wojciak <Code.AdamW78@gmail.com>
2023-01-06 10:01:08 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#define MLPACK_PRINT_INFO
+#define MLPACK_PRINT_WARN
+
+#include <iostream>
+#include "include/armadillo-11.4.3/include/armadillo"
+#include "include/mlpack-4.0.1/src/mlpack.hpp"
+
+using namespace arma;
+using namespace mlpack;
+using namespace mlpack::tree;
+
+
+int main() {
+    std::cout << "Success" << std::endl;
+    return 0;
+}
